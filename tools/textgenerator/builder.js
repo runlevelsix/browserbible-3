e39849c82b6e3297dc8799f01cecbd7de5689b4c
Jay Clark <jclark@maf.org>
2014-07-01 14:17:27 -0600

    adding mkdirp module and using it to make deep directories without failing. There was also some auto reformatting of the file.

--- tools/textgenerator/builder.js
+++ tools/textgenerator/builder.js
@@ -1,13 +1,14 @@
 // REQUIRE
-var 
+var
 	fs = require('fs'),
-	path = require('path'),	
+	path = require('path'),
 	jsdom = require("jsdom"),
 	$ = require('jquery')(jsdom.jsdom().createWindow()),
 	uglifyjs = require("uglify-js"),
 	uglifycss = require("uglifycss"),
 	jsp = require("uglify-js").parser,
-	pro = require("uglify-js").uglify;
+	pro = require("uglify-js").uglify
+	mkdirp = require("mkdirp");
 
 // START
 var
@@ -15,44 +16,44 @@ var
 	buildPath = path.join(rootPath, 'build'),
 	inputFilePath = path.join(rootPath, 'index.html'),
 	// change <script> to <scripty> since that means it won't get executed
-	html = fs.readFileSync(inputFilePath, 'utf8').replace(/script/gi,'scripty'),
-	doc = $( html ),
+	html = fs.readFileSync(inputFilePath, 'utf8').replace(/script/gi, 'scripty'),
+	doc = $(html),
 	scriptNodes = doc.find('scripty[src]'),
 	stylesheetNodes = doc.find('link[rel="stylesheet"]'),
-	
+
 	copyFiles = [],
-	scripts = [],	
+	scripts = [],
 	stylesheets = [],
-	
-	combinedCss = '',	
+
+	combinedCss = '',
 	minifiedCss = '',
 	outputCssPath = path.join(buildPath, 'build.css'),
-	outputCssPathMinified = path.join(buildPath, 'build.min.css'),		
+	outputCssPathMinified = path.join(buildPath, 'build.min.css'),
 
-	combinedScript = '',	
+	combinedScript = '',
 	minifiedScript = '',
 	outputJsPath = path.join(buildPath, 'build.js'),
-	outputJsPathMinified = path.join(buildPath, 'build.min.js');	
+	outputJsPathMinified = path.join(buildPath, 'build.min.js');
 
 
 // JAVASCIPT
 // find all script URLs
-scriptNodes.each(function(i, el) {	
+scriptNodes.each(function(i, el) {
 	var src = $(this).attr('src'),
 		build = $(this).attr('data-build');
 
 	if (build === 'copy') {
 		copyFiles.push(src);
 	} else {
-		scripts.push( src );
-	}	
+		scripts.push(src);
+	}
 });
 
 // combine scripts
 scripts.forEach(function(url) {
-	
+
 	var localPath = path.join(rootPath, url);
-	
+
 	try {
 		minifiedScript += uglifyjs.minify(localPath).code;
 	} catch (e) {
@@ -68,45 +69,45 @@ fs.writeFileSync(outputJsPathMinified, minifiedScript);
 
 // CSS
 // find all stylesheet URLs
-stylesheetNodes.each(function(i, el) {	
+stylesheetNodes.each(function(i, el) {
 	var href = $(this).attr('href'),
 		build = $(this).attr('data-build');
 
 	if (build === 'copy') {
 		copyFiles.push(href);
 	} else {
-		stylesheets.push( href );
-	}	
+		stylesheets.push(href);
+	}
 });
 
 
 // combine CSS
 stylesheets.forEach(function(url) {
-	
+
 	var localPath = path.join(rootPath, url);
-	
+
 	try {
 		minifiedCss += uglifycss.processFiles([localPath]);
 	} catch (e) {
 		console.log('error Css minifiy', localPath, e);
-	}	
+	}
 	combinedCss += fs.readFileSync(path.join(rootPath, url), 'utf8');
 });
 
 // write out
 fs.writeFileSync(outputCssPath, combinedCss);
-fs.writeFileSync(outputCssPathMinified, minifiedCss);  // TEMP: no compression
+fs.writeFileSync(outputCssPathMinified, minifiedCss); // TEMP: no compression
 
 // COPY 
 // copy remaining
 copyFiles.forEach(function(url) {
 	var fileIn = path.join(rootPath, url),
 		baseName = path.basename(url)
-		fileOut = path.join(buildPath, baseName),
-		
-		textIn = fs.readFileSync(fileIn, 'utf8');
-		
-	fs.writeFileSync(fileOut, textIn);		
+	fileOut = path.join(buildPath, baseName),
+
+	textIn = fs.readFileSync(fileIn, 'utf8');
+
+	fs.writeFileSync(fileOut, textIn);
 });
 
 
@@ -115,32 +116,35 @@ var copyFolders = ['css/fonts', 'css/images'];
 
 copyFolders.forEach(function(copyFolder) {
 	var folderIn = path.join(rootPath, copyFolder),
-		folderOut = path.join(buildPath, copyFolder).replace('css/','');
-		
+		folderOut = path.join(buildPath, copyFolder).replace('css', '');
+
+	// console.log("\nbuildPath: " + buildPath + "\ncopyFolder: " + copyFolder);
+	// console.log("\nfolderIn: " + folderIn + "\nfolderOut: " + folderOut);
+
 	copyRecursive(folderIn, folderOut);
 });
 
 function copyRecursive(folderIn, folderOut) {
-	
+
 	// create folder
 	if (!fs.existsSync(folderOut)) {
-		fs.mkdirSync(folderOut);
+		// console.log("copyRecursive: creating folder" + folderOut);
+		// fs.mkdirSync(folderOut); ////
+		mkdirp.sync(folderOut); // replacing fs.mkdirSync with mkdirp.sync to create any missing folders
 	}
 
 	// all subfolders and files
 	var subs = fs.readdirSync(folderIn);
-	
+
 	subs.forEach(function(data) {
 		var pathIn = path.join(folderIn, data);
-			pathOut = path.join(folderOut, data);
-		
+		pathOut = path.join(folderOut, data);
+
 		if (fs.statSync(pathIn).isFile()) {
-			fs.createReadStream(pathIn).pipe(fs.createWriteStream( pathOut ));			
+			fs.createReadStream(pathIn).pipe(fs.createWriteStream(pathOut));
 		} else {
 			copyRecursive(pathIn, pathOut);
-		}		
-	});	
-	
+		}
+	});
+
 }
-
-
